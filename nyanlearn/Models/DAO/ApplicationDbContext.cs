e2d9c302bffca2924cfd20b3f538f99dc69d1c1e
[Min-Khant-Maung] <[minkhant13003@gmail.com]>
2023-06-18 15:45:06 +0630

    updated

--- nyanlearn/Models/DAO/ApplicationDbContext.cs
+++ nyanlearn/Models/DAO/ApplicationDbContext.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace nyanlearn.Models.DAO
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        
+        
+        
+        public DbSet<TeacherCourses> TeacherCourses { get; set; }
+        public DbSet<ContactAnyQuery> ContactAnyQueries { get; set; }
+        public DbSet<NewStudentRegister> NewStudentRegisters { get; set; }
     }
 }
